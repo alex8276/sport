c055af0b6c49b34fd05be17edeadd8ef49540d65
alex8276 <nenes82@hotmail.com>
2017-09-06 20:08:25 +0200

    modif 2

--- seanceManager.class.php
+++ seanceManager.class.php
@@ -10,7 +10,10 @@ class seanceManager{
 	}
 
 	public function get($id){
-		$req = $this->_db->query('SELECT * FROM seance WHERE id = '.$id;);
+		$req = $this->_db->query('SELECT s.*,e.*,se.* FROM seance s
+			INNER JOIN serie se ON s.id = se.idSeance
+			INNER JOIN exercice e ON e.id = se.idExercice
+			WHERE s.id = '.$id);
 		$donnee = $req->fetch(PDO::FETCH_ASSOC);
 
 		return new seance($donnee);
